b9623732b2a74907dc4ba601ec575439ea1ad4da
Thomas O'Boyle <tjoboyle@Thomass-MacBook-Pro.local>
2018-09-17 22:23:27 -0400

    file cleanup, bug fix on runserver to properly track public/ dir and load images

--- runserver.js
+++ runserver.js
@@ -1,16 +1,23 @@
-const http = require('http'),
-      fs = require('fs'),
-      PORT = process.env.PORT || 5000;
+var http = require('http'),
+    express = require('express'),
+    /*fs = require('fs'),*/
+    app = express(),
+    /*bodyParser = require ('body-parser'),*/
+    PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-    fs.readFile('front_page.html', function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
+/*app.use (bodyParser ());*/
+var server = http.createServer(app);
+
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.engine('.html', require('ejs').__express);
+app.engine('.png', require('ejs').__express);
+
+
+app.get ('/', function (req, res){
+    console.log ('GET /');
+    res.render('front_page.html');
     });
-});
-
-server.use(express.static('public'));
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
